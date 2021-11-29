12611ad13a0ad9fa147c78dfe39689014fc415d5
ljacob23 <ljacob23@live.unc.edu>
2021-11-29 00:35:01 -0500

    frontend

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,18 +1,48 @@
 import './App.css';
-import React from 'react'
+import React, { Component} from 'react'
 import { Link } from 'react-router-dom';
+import pizzalogo from './pizzalogo2.jpeg';
+import pizzastore from './pizzastore.jpeg';
+// import account from './Account.js';
+import ReactDOM from "react-dom";
+import { BrowserRouter as Route, Switch } from "react-router-dom";
 
-function App() {
+
+
+
+
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>lastminute final project</h1>
-        <p>Sam, Lingling, Jackson, Hyunmo, Eric</p>
-        <p>test</p>
-        <Link to='/users'>Users</Link>
+       <header className="App-header">
+        <h1>Welcome to Pizza Press (Sôst) <img className= "photo" src={pizzalogo} alt="logo"/></h1>
+        
       </header>
+       <form className="form">
+        <label className="label">
+        username:
+        <input type="text" name="name" />
+        </label>
+        <label className="label">
+         password:
+        <input type="text" name="password" />
+        </label>
+        <input className = "button" type="submit" value="login" />
+        <p className="create">Don't have an account yet? 
+       <div>
+       <Link to='/Account'>Create an Account</Link>
+       </div>
+  
+         
+        </p>
+      </form>
+     
+     <div className="background">
+     <img className= "background-photo" src={pizzastore} alt="logo"/>
+     </div>
     </div>
   );
 }
 
-export default App;
+
+
