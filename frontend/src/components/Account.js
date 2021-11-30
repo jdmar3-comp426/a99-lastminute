5cfcb634e8826c4de56d9ac8dfab421cd03bbd5e
samrshi <samrshi@outlook.com>
2021-11-30 14:02:39 -0500

    Formatting and fix div inside p error

--- frontend/src/components/Account.js
+++ frontend/src/components/Account.js
@@ -1,62 +1,59 @@
+import React, { Component } from "react";
+import "./Account.css";
+import pizzaimage1 from "./pizzaimage1.jpeg";
+import pizzaimage2 from "./pizzaimage2.jpeg";
 
-  import React, { Component } from "react"
-  import './Account.css'
-  import pizzaimage1 from './pizzaimage1.jpeg'
-  import pizzaimage2 from './pizzaimage2.jpeg'
+class Account extends Component {
+  render() {
+    return (
+      <div className="full">
+        <img className="border_image" src={pizzaimage1} alt="image" />
+        <div className="container">
+          <h1 className="header">
+            <p className="title">Create your Account</p>
+          </h1>
 
-  class Account extends Component {
+          <div className="forms">
+            <div>
+              <form className="create_form">
+                <label className="label">
+                  username:
+                  <input className="input_box" type="text" name="name" />
+                </label>
+              </form>
+            </div>
 
-  
-     
-      render() {
-          return (
-            <div className="full">
-              <img className="border_image" src={pizzaimage1} alt="image"/>
-              <div className="container">
-                  <h1 className="header">
-                    <p className="title">Create your Account</p>
-                    </h1>
+            <div>
+              <form className="create_form">
+                <label className="label">
+                  password:
+                  <input className="input_box" type="text" name="password" />
+                </label>
+                <p className="pass_recs">
+                  *password must include at least 8 characters, a number
+                  character, and a special character
+                </p>
+              </form>
+            </div>
 
-                    <div className="forms">
+            <div>
+              <form className="create_form">
+                <label className="label">
+                  Re-enter password:
+                  <input className="input_box" type="text" name="password" />
+                </label>
+              </form>
+            </div>
+          </div>
+          <button className="button1" href="#">
+            Submit
+          </button>
 
-                      <div> <form className="create_form">
-                      <label className="label">
-                      username:
-                      <input className="input_box" type="text" name="name" />
-                      </label>
-                    </form>
-                    </div>
-                   
-                    <div>
-                    <form className="create_form">
-                      <label className="label">
-                      password:
-                      <input className="input_box" type="text" name="password" />
-                      
-                      </label>
-                      <p className="pass_recs">*password must include at least 8 characters, a number character, and a special character</p>
-                    </form>
-                    </div>
-
-
-                    <div>
-                    <form className="create_form">
-                      <label className="label">
-                      Re-enter password:
-                      <input className="input_box" type="text" name="password" />
-                      </label>
-
-                    </form>
-                    </div>
-                    
-                    </div>
-                    <button className="button1" href="#">Submit</button>
-                    
-                    <img className="border_image2" src={pizzaimage2} alt="image"/>
-              </div>
-              </div>
-          )
-      }
+          <img className="border_image2" src={pizzaimage2} alt="image" />
+        </div>
+      </div>
+    );
   }
-  
-  export default Account
+}
+
+export default Account;
