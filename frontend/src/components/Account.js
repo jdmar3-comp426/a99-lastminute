6a7883ccf00e9c87b06b6495d063c32ccc5a5901
samrshi <samrshi@outlook.com>
2021-12-01 16:43:39 -0500

    Added functionality for create account button

--- frontend/src/components/Account.js
+++ frontend/src/components/Account.js
@@ -4,7 +4,58 @@ import pizzaimage1 from "./pizzaimage1.jpeg";
 import pizzaimage2 from "./pizzaimage2.jpeg";
 
 class Account extends Component {
-  state = { username: "", password: "", passwordAgain: "" }
+  constructor(props) {
+    super(props);
+
+    this.state = { username: "", password: "", passwordAgain: "" };
+    this.handleUsernameChange = this.handleUsernameChange.bind(this);
+    this.handlePasswordChange = this.handlePasswordChange.bind(this);
+    this.handlePasswordAgainChange = this.handlePasswordAgainChange.bind(this);
+    this.attemptCreateAcount = this.attemptCreateAcount.bind(this);
+  }
+
+  attemptCreateAcount() {
+    // check password equality
+    if (this.state.password !== this.state.passwordAgain) {
+      alert("Passwords do not match");
+      return;
+    }
+
+    // Actually try to make account
+    const requestOptions = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        username: this.state.username,
+        password: this.state.password,
+      }),
+    };
+
+    fetch("/app/users/create", requestOptions)
+      .then((response) => response.json())
+      .then((json) => {
+        if (json.result === "success") {
+          // TODO: go into game play
+          alert(json.message);
+        } else {
+          alert(json.message);
+        }
+      });
+  }
+
+  handleUsernameChange(event) {
+    this.setState({ username: event.target.value });
+  }
+
+  handlePasswordChange(event) {
+    this.setState({ password: event.target.value });
+  }
+
+  handlePasswordAgainChange(event) {
+    this.setState({ passwordAgain: event.target.value });
+  }
 
   render() {
     return (
@@ -20,12 +71,13 @@ class Account extends Component {
               <form className="create_form">
                 <label className="label">
                   username:
-                  
-                  <TextField value={this.state.username} label="Enter Username" onChange={(e) => {
-                    this.setState({ username: e.target.value })
-                  }}/>
-
-                  <input className="input_box" type="text" name="name" />
+                  <input
+                    className="input_box"
+                    type="text"
+                    name="username"
+                    value={this.state.username}
+                    onChange={this.handleUsernameChange}
+                  />
                 </label>
               </form>
             </div>
@@ -34,7 +86,13 @@ class Account extends Component {
               <form className="create_form">
                 <label className="label">
                   password:
-                  <input className="input_box" type="text" name="password" />
+                  <input
+                    className="input_box"
+                    type="text"
+                    name="password"
+                    value={this.state.password}
+                    onChange={this.handlePasswordChange}
+                  />
                 </label>
                 <p className="pass_recs">
                   *password must include at least 8 characters, a number
@@ -47,12 +105,22 @@ class Account extends Component {
               <form className="create_form">
                 <label className="label">
                   Re-enter password:
-                  <input className="input_box" type="text" name="password" />
+                  <input
+                    className="input_box"
+                    type="text"
+                    name="password_again"
+                    value={this.state.passwordAgain}
+                    onChange={this.handlePasswordAgainChange}
+                  />
                 </label>
               </form>
             </div>
           </div>
-          <button className="button1" href="#">
+          <button
+            className="button1"
+            href="#"
+            onClick={this.attemptCreateAcount}
+          >
             Submit
           </button>
 
