a8f98851353234439cc19841831b912db6ad906c
samrshi <samrshi@outlook.com>
2021-12-03 16:47:36 -0500

    make changes to make deployment hopefully possible

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,26 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
+const path = require('path')
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-const usersRouter = require('./Users/UsersRouter')
-app.use("/app/users", usersRouter)
+// Establish users endpoints
+const usersRouter = require("./Users/UsersRouter");
+app.use("/app/users", usersRouter);
 
-const historyRouter = require('./History/HistoryRouter')
-app.use("/app/history", historyRouter)
+// Establish history endpoints
+const historyRouter = require("./History/HistoryRouter");
+app.use("/app/history", historyRouter);
 
-app.listen(3001, function() {
-    console.log("express server is running on port 3001")
-})
+// If we get a request on an endpoint not already handled, send React files
+app.use(express.static(path.resolve(__dirname, "../frontend/build")));
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "../frontend/public", "index.html"));
+});
+
+const PORT = process.env.PORT || 3001;
+
+app.listen(3001, function () {
+  console.log("express server is running on port " + PORT);
+});
