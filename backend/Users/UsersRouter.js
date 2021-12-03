8869de84c64e64240c61796e12e920447586812c
dowden19 <dowden19@live.unc.edu>
2021-12-03 15:21:00 -0500

    switching to balance

--- backend/Users/UsersRouter.js
+++ backend/Users/UsersRouter.js
@@ -113,7 +113,7 @@ router.post("/login", (req, res) => {
 
 
 router.get("/getbal/:username", (req, res) => {
-    const stmt = db.prepare("SELECT bal FROM userinfo WHERE username = ?").get(req.params.username);
+    const stmt = db.prepare("SELECT balance FROM userinfo WHERE username = ?").get(req.params.username);
     res.json({result: stmt.balance});
 })
 router.get("/getcpp/:username", (req, res) => {
@@ -121,12 +121,11 @@ router.get("/getcpp/:username", (req, res) => {
     res.json({result: stmt.cpp});
 })
 router.patch("/setbal/:username", (req, res) => {	
-	const stmt = db.prepare("UPDATE userinfo SET balance = COALESCE(?,balance), cpp = COALESCE(?,cpp) WHERE username = ?");
-	const info = stmt.run(req.body.balance, req.body.cpp, req.params.username);
+	const stmt = db.prepare("UPDATE userinfo SET balance = COALESCE(?,balance) WHERE username = ?");
+	const info = stmt.run(req.body.balance, req.params.username);
 	res.status(200).json({
         "message": info.changes + " record updated: user " + req.params.username + " (200)",
-        "balance": req.body.balance,
-        "cpp": req.body.cpp
+        "balance": req.body.balance
     });
 });
 
