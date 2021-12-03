8c97ff776b9e8aeb71cc9e4ccc6765d3cb6c509e
dowden19 <dowden19@live.unc.edu>
2021-12-03 18:51:09 -0500

    added spending and revenue

--- frontend/src/components/Game/Game.js
+++ frontend/src/components/Game/Game.js
@@ -4,10 +4,10 @@ import "./Game.css";
 export default function Game({ username }) {
   const [balance, setBal] = useState(0);
   const [cpp, setCPP] = useState(0);
+  const [spending, setSpending] = useState(0);
+  const [revenue, setRevenue] = useState(0);
   const [pepperoni, setPepperoni] = useState(0);
   const [firstLoad, setFirstLoad] = useState(true);
-
- 
   
   const getBal = () => {
     fetch("/app/users/getbal/" + username)
@@ -30,42 +30,22 @@ export default function Game({ username }) {
         setPepperoni(json.result);
       });
   };
+  const getSpending = () => {
+    fetch("/app/users/getspending/" + username)
+      .then((res) => res.json())
+      .then((json) => {
+        setSpending(json.result);
+      });
+  };
+  const getRevenue = () => {
+    fetch("/app/users/getrevenue/" + username)
+      .then((res) => res.json())
+      .then((json) => {
+        setRevenue(json.result);
+      });
+  };
 
-
-  // const updateBal = () => {
-  //   var requestOptions = {
-  //     method: "PATCH",
-  //     headers: {
-  //       "Content-Type": "application/json",
-  //     },
-  //     body: JSON.stringify({
-  //       balance: balance + cpp,
-  //     }),
-  //   };
-  //   fetch("/app/users/setbal/" + username, requestOptions)
-  //     .then((res) => res.json())
-  //     .then((json) => setBal(json.balance));
-  // };
-  // const updateCPP = () => {
-  //   console.log("hi i'm updating")
-  //   var requestOptions = {
-  //     method: "PATCH",
-  //     headers: {
-  //       "Content-Type": "application/json",
-  //     },
-  //     body: JSON.stringify({
-  //       cpp: 10 + 3*pepperoni,
-  //     }),
-  //   };
-  //   fetch("/app/users/setcpp/" + username, requestOptions)
-  //     .then((res) => res.json())
-  //     .then((json) => {
-  //       setCPP(json.cpp)
-  //       console.log(json.cpp)
-  //     });
-  // };
-
-  const setGameState = (balance, cpp, pepperoni) => {
+  const setGameState = (balance, cpp, spending, revenue, pepperoni) => {
     var requestOptions = {
       method: "PATCH",
       headers: {
@@ -74,41 +54,27 @@ export default function Game({ username }) {
       body: JSON.stringify({
         balance: balance,
         cpp: cpp,
+        spending: spending,
+        revenue: revenue,
         pepperoni: pepperoni,
       }),
     };
     fetch("/app/users/setgamestate/" + username, requestOptions)
       .then((res) => res.json())
       .then((json) => {
-        console.log("i'm here")
         setBal(json.balance)
         setCPP(json.cpp)
+        setSpending(json.spending)
+        setRevenue(json.revenue)
         setPepperoni(json.pepperoni)
       })
   }
 
-  // const updatePepperoni = () => {
-  //   var requestOptions = {
-  //     method: "PATCH",
-  //     headers: {
-  //       "Content-Type": "application/json",
-  //     },
-  //     body: JSON.stringify({
-  //       pepperoni: 1,
-  //     }),
-  //   };
-  //   fetch("/app/users/setpepperoni/" + username, requestOptions)
-  //     .then((res) => res.json())
-  //     .then((json) => {
-  //       setPepperoni(json.pepperoni)
-  //       updateCPP()
-  //     })
-  // };
-
-
   if (firstLoad) {
     getBal();
     getCPP();
+    getSpending();
+    getRevenue();
     getPepperoni();
     setFirstLoad(false);
   }
@@ -118,16 +84,16 @@ export default function Game({ username }) {
       <div className="welcome">Welcome to {username}'s pizzeria</div>
 
       <div className="wrapper">
-        <button className="pizza_button" onClick={() => setGameState(balance+cpp,cpp,pepperoni)}></button>
+        <button className="pizza_button" onClick={() => setGameState(balance+cpp, cpp, spending, revenue+cpp, pepperoni)}></button>
 
         <div className="bank">
           BANK
           <p>Balance: $ {balance}</p>
 
           <div className="text">
-            <p>Price per Pizza: $ {cpp}</p>
-            <p>Total Spending:</p>
-            <p>Revenue:</p>
+            <p>Price per Pizza: ${cpp}</p>
+            <p>Total Spending: ${spending}</p>
+            <p>Revenue: ${revenue}</p>
           </div>
         </div>
       </div>
@@ -135,8 +101,7 @@ export default function Game({ username }) {
       <div className="bottomwrapper">
         <div className="store">
           <h1 className="oven_header">Store</h1>
-          <button className="pep" onClick={() => setGameState(balance,cpp+3,1)}>Pepperoni: $1000 {pepperoni}</button>
-
+          <button className="pep" onClick={() => setGameState(balance-1000, cpp+3, spending+1000, revenue, 1)}>Pepperoni: $1000 {pepperoni}</button>
           <button className="oven">Mushrooms: $1200</button>
           <button className="oven">Peppers: $1400</button>
           <button className="oven">Sausage: $1600</button>
